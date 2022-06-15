ec75b34947b2e657c53156011ee192127a0db4a3
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2022-06-15 17:46:54 -0500

    Update parse.py

--- parse.py
+++ parse.py
@@ -3,6 +3,6 @@ from utils import *
 def parse():
     for i in range(1, end_z-start_z, 2):
         blocks = get_blocks(MC, start_x+1, start_y+1, start_z+i, end_x-1, start_y+1, start_z+i)
-        print(len(blocks))
+        print(blocks)
 
 parse()
