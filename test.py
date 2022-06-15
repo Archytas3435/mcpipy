55cbaa671c5b4be779ad7f2fab3e34aef76267ec
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2022-06-15 15:12:07 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ from mcpi.minecraft import Minecraft
 
 mc = minecraft.Minecraft.create()
 mc = Minecraft.create()
-
+print("a")
 for x in range(1602, 1611):
     for y in range(-5, 5):
         for z in range(-348, -339):
-            print(mc.setBlock(x, y, z, 17))
+            mc.setBlock(x, y, z, 17)
