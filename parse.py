3d52c5ce8dc2bfe5e10cf4c1a61695813d471119
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2022-06-15 17:31:51 -0500

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,5 +1,7 @@
 from utils import *
 
+mc = Minecraft.create()
+
 def parse():
     for i in range(0, end_z-start_z, 2):
         blocks = get_blocks(mc, start_x+1, start_y+1, start_z+i, end_x-1, start_y+1, start_z+i)
