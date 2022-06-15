75fd7f21bef068b263fd774b24c6d504ceae7c4c
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2022-06-15 17:26:18 -0500

    Create parse.py

---
+++ parse.py
@@ -0,0 +1,7 @@
+
+def parse():
+    for i in range(0, end_z-start_z, 2):
+        blocks = get_blocks(start_x+1, start_y+1, start_z+i, end_x-1, start_y+1, start_z+i)
+        print(blocks)
+        
+parse()
