7ea28f4b5919b7fedfbd981337983f4f8a16fa0b
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2022-06-15 17:14:36 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from mcpi.minecraft import Minecraft
 from mcpi import block
 from qiskit import QuantumCircuit, Aer, assemble
+from time import sleep
 
 mc = Minecraft.create()
 
@@ -16,9 +17,20 @@ def get_blocks(x0, y0, z0, x1, y1, z1):
 air_block = block.Block(0)
 border_block = block.Block(35, 0)
 stripe_block = block.Block(35, 14)
+hadamard_block = block.Block(35, 1)
+not_block = block.Block(35, 2)
+cnot_start_block = block.Block(35, 3)
+cnot_end_block = block.Block(35, 4)
+measure_block = block.Block(35, 15)
+qubit_0_block = block.Block(35, 5)
+qubit_25_block = block.Block(35, 6)
+qubit_50_block = block.Block(35, 7)
+qubit_75_block = block.Block(35, 8)
+qubit_100_block = block.Block(35, 9)
 start_x, end_x = 400, 420
 start_y, end_y = 0, 10
 start_z, end_z = 600, 615
+
 def start():
     change_blocks(start_x-10, start_y-10, start_z-10, end_x+10, end_y+10, end_z+10, air_block)
     change_blocks(start_x, start_y, start_z, start_x, end_y, end_z, border_block)
@@ -31,5 +43,14 @@ def start():
         change_blocks(start_x, start_y, start_z+i, end_x, start_y, start_z+i, stripe_block)
     teleport((start_x+end_x)//2, (start_y+end_y)//2, (start_z+end_z)//2)
 
-start()
+def parse():
+    for i in range(0, end_z-(start_z+1), 2):
+        blocks = get_blocks(start_x+1, start_y+1, start_z+i, end_x-1, start_y+1, start_z+i)
+        print(blocks)
 
+start()
+while True:
+    message = mc.player.pollChatPosts()
+    if "start" in message:
+        parse()
+    sleep(0.1)
