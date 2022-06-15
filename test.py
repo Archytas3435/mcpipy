43fd6fdc622a904ed59d3c93ff26736c574b064d
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2022-06-15 15:31:50 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ from mcpi import block
 mc = Minecraft.create()
 print("a")
 mc.player.setPos(1600, 8, -400)
-mc.setBlocks(1600, 10, -400, 1602, 12, -398, block.OBSIDIAN)
+mc.setBlocks(1600, 10, -400, block.OBSIDIAN)
 
