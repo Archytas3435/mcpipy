4faedefac0852bb5662557069224189681e16608
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2022-06-15 16:25:40 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from mcpi.minecraft import Minecraft
 from mcpi import block
+from qiskit import QuantumCircuit, Aer, assemble
 
 mc = Minecraft.create()
 
@@ -13,14 +14,19 @@ def get_blocks(x0, y0, z0, x1, y1, z1):
     return mc.getBlocks(x0, y0, z0, x1, y1, z1, block_type)
 
 block = block.Block(35, 0)
+start_x, end_x = 400, 420
+start_y, end_y = 0, 10
+start_z, end_z = 600, 620
 def start():
-    change_blocks(400, 0, 600, 400, 10, 620, block)
-    change_blocks(400, 0, 600, 420, 0, 620, block)
-    change_blocks(400, 0, 600, 420, 10, 600, block)
-    change_blocks(420, 0, 600, 420, 10, 620, block)
-    change_blocks(400, 0, 620, 420, 10, 620, block)
-    change_blocks(420, 0, 600, 420, 10, 620, block)
-    teleport(410, 5, 610)
+    change_blocks(start_x, start_y, start_z, start_x, end_y, end_z, block)
+    change_blocks(start_x, start_y, start_z, end_x, start_y, end_z, block)
+    change_blocks(start_x, start_y, start_z, end_x, end_y, start_z, block)
+    change_blocks(end_x, start_y, start_z, end_x, end_y, end_z, block)
+    change_blocks(start_x, start_y, end_z, end_x, end_y, end_z, block)
+    change_blocks(end_x, start_y, start_z, end_x, end_y, end_z, block)
+    for i in range(0, end_z-(start_z+1), 2):
+        change_blocks(start_x, start_y, start_z+i, end_x, start_y, start_z+i, block)
+    teleport((start_x+end_x)//2, (start_y+end_y)//2, (start_y+end_y)//2)
 
 start()
 
