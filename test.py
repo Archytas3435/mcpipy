16d792a90473e88112da741b84c0e31d174b0c51
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2022-06-15 14:52:18 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,7 @@ from mcpi.minecraft import Minecraft
 
 mc = Minecraft.create()
 
-print(mc.getBlocks())
+for x in range(1602, 1611):
+    for y in range(4, 8):
+        for z in range(-348, -339):
+            print(mc.getBlock(x, y, z))
