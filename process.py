5c75e32ea1c37063f5f90141a295983cf0786ef3
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2022-06-15 18:30:45 -0500

    Update process.py

--- process.py
+++ process.py
@@ -5,4 +5,5 @@ import numpy as np
 
 current_state = parse()
 
-qc = QuantumCircuit((current_state==qubit_0_block.id or current_state==qubit_25_block.id or current_state==qubit_50_block.id or current_state==qubit_75_block.id or current_state==qubit_100_block.id).sum())
+qc = QuantumCircuit((current_state==qubit_0_block.id).sum()+(current_state==qubit_25_block.id).sum()+(current_state==qubit_50_block.id).sum()+(current_state==qubit_75_block.id).sum()+(current_state==qubit_100_block.id).sum())
+
