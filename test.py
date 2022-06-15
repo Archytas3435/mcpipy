3644a7648ef4771259034fc7dd2ac8b87c3f90fc
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2022-06-15 15:03:58 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
-from mcpi.minecraft import Minecraft
-import mcpi
+import mcpi.minecraft as minecraft
 
-mc = Minecraft.create()
+mc = minecraft.Minecraft.create()
 
-for x in range(1602, 1611):
-    for y in range(0, 10):
-        for z in range(-348, -339):
-            mc.spawnEntity(x, y, z, x*y*z)
+mc.postToChat("Hello PythonCraft World")
