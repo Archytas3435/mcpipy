f033711aaa2f9f080afb52b70d526255573278b5
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2022-06-15 18:31:49 -0500

    Update parse.py

--- parse.py
+++ parse.py
@@ -4,5 +4,3 @@ import numpy as np
 def parse():
     blocks = [get_blocks(MC, start_x+1, start_y+1, start_z+i, end_x-1, start_y+1, start_z+i) for i in range(1, end_z-start_z, 2)]
     return np.array(blocks)
-
-print(parse())
