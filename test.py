b8e1863a0c673c8c3ff6f798118f0311fc60992e
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2022-06-15 14:57:36 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 from mcpi.minecraft import Minecraft
+import mcpi
 
 mc = Minecraft.create()
 
 for x in range(1602, 1611):
     for y in range(4, 6):
         for z in range(-348, -339):
-            print(mc.getBlockWithData(x, y, z))
+            print(mc.setBlock(x, y, z, mcpi.block.AIR))
