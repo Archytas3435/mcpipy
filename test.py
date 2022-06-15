68c7f359c66a78040250e7149b0c88c68a66330e
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2022-06-15 15:47:09 -0500

    Update test.py

--- test.py
+++ test.py
@@ -8,13 +8,13 @@ def teleport(x, y, z):
 
 def change_blocks(x0, y0, z0, x1, y1, z1, block_type):
     mc.setBlocks(x0, y0, z0, x1, y1, z1, block_type)
-    
+
 def get_blocks(x0, y0, z0, x1, y1, z1):
     return mc.getBlocks(x0, y0, z0, x1, y1, z1, block_type)
-    
+
 def start():
-    change_blocks(-10, -10, -10, 10, 10, 10, block.AIR)
-    teleport(0, 0, 0)
+    change_blocks(400, -10, 600, 420, 10, 620, block.AIR)
+    teleport(410, 0, 610)
 
 start()
 
