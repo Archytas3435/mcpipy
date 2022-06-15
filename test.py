ec33cc1970fc33d587afbc36ed5dcf996c0eebdf
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2022-06-15 16:05:57 -0500

    Update test.py

--- test.py
+++ test.py
@@ -12,13 +12,14 @@ def change_blocks(x0, y0, z0, x1, y1, z1, block_type):
 def get_blocks(x0, y0, z0, x1, y1, z1):
     return mc.getBlocks(x0, y0, z0, x1, y1, z1, block_type)
 
+block = block.Block(35, 0)
 def start():
-    change_blocks(400, 0, 600, 400, 10, 620, block.QUARTZ_BLOCK)
-    change_blocks(400, 0, 600, 420, 0, 620, block.QUARTZ_BLOCK)
-    change_blocks(400, 0, 600, 420, 10, 600, block.QUARTZ_BLOCK)
-    change_blocks(420, 0, 600, 420, 10, 620, block.QUARTZ_BLOCK)
-    change_blocks(400, 0, 620, 420, 10, 620, block.QUARTZ_BLOCK)
-    change_blocks(420, 0, 600, 420, 10, 620, block.QUARTZ_BLOCK)
+    change_blocks(400, 0, 600, 400, 10, 620, block)
+    change_blocks(400, 0, 600, 420, 0, 620, block)
+    change_blocks(400, 0, 600, 420, 10, 600, block)
+    change_blocks(420, 0, 600, 420, 10, 620, block)
+    change_blocks(400, 0, 620, 420, 10, 620, block)
+    change_blocks(420, 0, 600, 420, 10, 620, block)
     teleport(410, 0, 610)
 
 start()
