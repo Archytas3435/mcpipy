5b9d0b586e9c8fc2222d7505ffc0da19a2742825
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2022-06-15 15:44:19 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,19 @@ from mcpi.minecraft import Minecraft
 from mcpi import block
 
 mc = Minecraft.create()
-print("a")
-mc.player.setPos(1600, 8, -400)
-mc.setBlocks(1600, 10, -400, 1650, 15, -360, block.CACTUS)
+
+def teleport(x, y, z):
+    mc.player.setPos(x, y, z)
+
+def change_blocks(x0, y0, z0, x1, y1, z1, block_type):
+    mc.setBlocks(x0, y0, z0, x1, y1, z1, block_type)
+    
+def get_blocks(x0, y0, z0, x1, y1, z1):
+    return mc.getBlocks(x0, y0, z0, x1, y1, z1, block_type)
+    
+def start():
+    change_blocks(-10, -10, -10, 10, 10, 10, block.AIR)
+    teleport(0, 0, 0)
+
+start()
 
