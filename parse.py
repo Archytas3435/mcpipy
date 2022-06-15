0269b459d568e5a4eb01a272819408ab2f9af959
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2022-06-15 17:51:17 -0500

    Update parse.py

--- parse.py
+++ parse.py
@@ -2,7 +2,7 @@ from utils import *
 import numpy as np
 
 def parse():
-    blocks = [get_blocks(mc, start_x+1, start_y+1, start_z+i, end_x-1, start_y+1, start_z+i) for i in range(1, end_z-start_z, 2)]
+    blocks = [get_blocks(MC, start_x+1, start_y+1, start_z+i, end_x-1, start_y+1, start_z+i) for i in range(1, end_z-start_z, 2)]
     return np.array(blocks)
 
 print(parse())
