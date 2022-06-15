83f3828fb2d4d6f4ce1f29c78fc81004e101340f
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2022-06-15 15:00:48 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,4 +6,4 @@ mc = Minecraft.create()
 for x in range(1602, 1611):
     for y in range(0, 10):
         for z in range(-348, -339):
-            mc.setBlock(x, y, z, mcpi.block.AIR)
+            mc.spawnEntity(x, y, z, x*y*z)
