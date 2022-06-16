66052afc1120b1d0f6399e3fc0db854b69d3cdda
Ubuntu <ubuntu@ip-172-31-8-248.ec2.internal>
2022-06-16 02:58:59 +0000

    Added python config

--- setupServer.sh
+++ setupServer.sh
@@ -8,10 +8,16 @@ java -version
 
 sudo apt install software-properties-common -y 
 
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 
 sudo apt update -y
 
 sudo apt install python3.8 -y
 
-python --version
+sudo apt install -y python3-pip
+
+python --versio
+
+pip install -r requirements.txt
+
+
