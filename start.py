c15d4bec4378dccecfa0372b635c4042f93296df
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2022-06-15 20:20:53 -0500

    Update start.py

--- start.py
+++ start.py
@@ -11,6 +11,9 @@ def start():
     change_blocks(MC, end_x, start_y, start_z, end_x, end_y, end_z, border_block)
     for i in range(1, end_z-start_z, 2):
         change_blocks(MC, start_x, start_y, start_z+i, end_x, start_y, start_z+i, stripe_block)
+    for a, b, c, d in ((start_x, end_x, start_z, start_z), (start_x, end_x, end_z, end_z), (start_x, start_x, start_z, end_z), (end_x, end_x, start_z, end_z)):
+        change_blocks(MC, a, 4, c, b, 4, d, glow_block)
+        
     teleport(MC, (start_x+end_x)//2, start_y+2, end_z-2)
 
 start()
