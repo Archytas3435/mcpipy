8e6d98c8208a6e87df751cc0c488515d9dd3b33c
Ubuntu <ubuntu@ip-172-31-8-248.ec2.internal>
2022-06-16 02:44:25 +0000

    Changed bash script

--- setupServer.sh
+++ setupServer.sh
@@ -6,3 +6,12 @@ sudo apt-get install openjdk-8-jdk -y
 
 java -version
 
+sudo apt install software-properties-common -y 
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+
+sudo apt update -y
+
+sudo apt install python3.8 -y
+
+python --version
